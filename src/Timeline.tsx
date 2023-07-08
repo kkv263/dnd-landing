41429c61bca1468ad4087ea44854fd04f28b3dd6
Kevin Vu <kevin.vu@crometrics.com>
2023-07-07 20:52:53 -0500

    timeline wip

--- src/Timeline.tsx
+++ src/Timeline.tsx
@@ -1,14 +1,23 @@
 import './styles/Timeline.scss'
+import { useInView } from 'react-intersection-observer';
+
 
 const Timeline = () => {
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0,
+  });
+
 
   return (
     <section className="timeline">
-      <div className="timeline__line"></div>
+      <div className="timeline__line">
+        <span className='timeline__step'>create</span>
+      </div>
       <div className="timeline__section">
         <div className="container">
           <div className="timeline__content">
-            <h2 className="timeline__headline">step 1: create a character</h2>
+            <h2 className="timeline__headline" ref={ref}>{inView ? 'in view' : 'not in view'} step 1: create a character</h2>
             <div className="timeline__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum ratione provident dolores labore placeat? Quaerat, ex odio a ut optio similique pariatur quod autem corporis non aut unde ipsa voluptas.</div>
           </div>
         </div>
